26a93914618e03be9abc1dcfb62301a9ae5082f1
Binyamin Friedman <binyaminf22@gmail.com>
2026-03-03 11:32:23 -0500

    macos installer refinement

---
+++ Releases/macOS/dmg_settings.py
@@ -0,0 +1,14 @@
+volume_name = "Install Just a Sample"
+icon = "Releases/macOS/icon.icns"
+format = "UDBZ"
+
+window_rect = ((0, 0), (600, 380))
+background = "#fdf6c3"
+icon_size = 200
+text_size = 16
+
+files = ["dmg_root/Just a Sample.pkg"]
+
+icon_locations = {"Just a Sample.pkg": (300, 0)}
+
+hide_extensions = ["Just a Sample.pkg"]
